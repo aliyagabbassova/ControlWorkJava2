c2ffc5ab047156d60a02aad88ea44528c9246018
aliya <aliya.gabbassova1911@gmail.com>
2023-06-19 15:10:34 +0600

    Toy store is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ToyStore toyStore = new ToyStore();
+        toyStore.addToy(new ToyStore.Toy(1, "Мяч", 10, 20));
+        toyStore.addToy(new ToyStore.Toy(2, "Кукла", 5, 30));
+        toyStore.addToy(new ToyStore.Toy(3, "Конструктор", 8, 50));
+        toyStore.addToy(new ToyStore.Toy(4, "Машинка", 15, 40));
+        toyStore.addToy(new ToyStore.Toy(5, "Плюшевый медведь", 12, 25));
+        toyStore.addToy(new ToyStore.Toy(6, "Книжка", 20, 15));
+        toyStore.addToy(new ToyStore.Toy(7, "Пазл", 7, 35));
+        toyStore.addToy(new ToyStore.Toy(8, "Кубики", 10, 30));
+        toyStore.addToy(new ToyStore.Toy(9, "Кукольный домик", 3, 45));
+        toyStore.addToy(new ToyStore.Toy(10, "Футбольный мяч", 6, 20));
+
+        ToyStore.Toy randomToy = toyStore.getRandomToy();
+        if (randomToy != null) {
+            System.out.println("Поздравляем, вы выиграли игрушку:");
+            System.out.println("ID: " + randomToy.getId());
+            System.out.println("Название: " + randomToy.getName());
+        } else {
+            System.out.println("Игрушки закончились :(");
+
+        }
+        ToyWrite toyWrite = new ToyWrite();
+        toyWrite.writeToText(toyStore);
     }
+
 }
--- src/main/java/org/example/ToyWrite.java
+++ src/main/java/org/example/ToyWrite.java
@@ -1,12 +1,12 @@
 package org.example;
-
-public class ToyWtite {
-    public static void main(String[] args) {
+import java.io.*;
+public class ToyWrite {
+    public void writeToText (ToyStore toy) {
         File myFile = new File("text.txt");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(myFile, true));
-            writer.write("everything is good\n");
-            writer.write("beautiful flower\n");
+            writer.write(toy.toString());
+            writer.write(",\n");
             writer.flush();
             writer.close();
         } catch (IOException ex) {
